6073a6d2147db61f5707de4cf67c8f99c4169923
jason <jie-email@jie-trancender.org>
2018-03-01 17:12:00 +0800

    update login

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+	attr_accessor :remember_token
+
 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+[a-z\d\-.]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
 
 	before_save { email.downcase! }
@@ -10,8 +12,26 @@ class User < ApplicationRecord
 	has_secure_password
 	validates :password, presence: true, length: { minimum: 6 }
 
-	def User.degest(str)
+	def self.degest(str)
 		cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
 		BCrypt::Password.create(str, cost: cost)
+	end
+
+	def self.new_token
+		SecureRandom.urlsafe_base64
+	end
+
+	def remember
+		self.remember_token = User.new_token
+		update_attribute(:remember_digest, User.degest(remember_token))
+	end
+
+	def authenticated?(remember_token)
+		return false if remember_digest.nil?
+		BCrypt::Password.new(remember_digest).is_password?(remember_token)
+	end
+
+	def forget
+		update_attribute(:remember_digest, nil)
 	end
 end
