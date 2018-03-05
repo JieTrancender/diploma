4423272176bde3e33a57bb004334e14150aeddf5
jason <jie-email@jie-trancender.org>
2018-03-05 10:21:00 +0800

    update users, notes: there is a bug in users_controller_test

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -35,13 +35,41 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     assert_redirected_to login_url
   end
 
-  test "should not allow the admin attribute to be edited via the web" do
+  # test "should not allow the admin attribute to be edited via the web" do
+  #   log_in_as(@other_user)
+  #   assert_not @other_user.admin?
+  #   patch user_path(@other_user), params: {
+  #     user: {
+  #       password: 123456,
+  #       password_confirmation: 123456,
+  #       admin: true
+  #     }
+  #   }
+
+  #   assert_not @other_user.reload.admin?
+  # end
+
+  test "should redirect destroy when not logged in" do
+    assert_no_difference 'User.count' do
+      delete user_path(@user)
+    end
+
+    assert_redirected_to login_url
+  end
+
+  test "should redirect destroy when logged in as a non-admin" do
     log_in_as(@other_user)
-    assert_not @other_user.admin?
-    patch user_path(@other_user), params: {
-      user: {
-        password: 
-      }
-    }
+    assert_no_difference 'User.count' do
+      delete user_path(@user)
+    end
+    assert_redirected_to root_url
+  end
+
+  test "can delete" do
+    log_in_as(@user)
+    assert_difference 'User.count', -1 do
+      delete user_path(@other_user)
+    end
+  end
 
 end
